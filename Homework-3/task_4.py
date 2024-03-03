adb65c9a73ae6929bc8edd00e06ff4b866473d5f
Mari <marisisoeva@gmail.com>
2024-03-03 19:15:51 +0400

    finished tasks

--- Homework-3/task_4.py
+++ Homework-3/task_4.py
@@ -1,12 +1,10 @@
 from datetime import datetime
-import requests
-import forex_python
-from forex_python.converter import CurrencyRates
+
 from forex_python.bitcoin import BtcConverter
 purchase_date = input('Please type purchase date (dd/mm/yyyy): ')
 purchase_price = int(input('How much did you pay in USD?: '))
 
-b = BtcConverter() # force_decimal=True to get Decimal rates
+b = BtcConverter()
 current_price = b.get_latest_price('USD')
 date_object = datetime.strptime(purchase_date, "%d/%m/%Y")
 old_price = b.get_previous_price('USD', date_object)
@@ -16,5 +14,6 @@ amount_owned = purchase_price / old_price
 current_value = amount_owned * current_price
 
 net_income = current_value - purchase_price
-print('You have won $ ', net_income)
+print('Your net income is $ ', net_income)
+
 
