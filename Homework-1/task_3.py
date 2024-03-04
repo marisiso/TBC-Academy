7726a2d695179dbf1c5ea9f316d6f3fd03e52a95
Mari <marisisoeva@gmail.com>
2024-03-04 18:28:31 +0400

    Fixed code

--- Homework-1/task_3.py
+++ Homework-1/task_3.py
@@ -7,12 +7,7 @@ s = per/2
 area = math.sqrt(s*(s-a)*(s-b)*(s-c))
 
 if a < 0 or b < 0 or c < 0:
-    print("Please enter positive numbers")
-    a = int(input("Enter triangle side a = "))
-    b = int(input("Enter triangle side b = "))
-    c = int(input("Enter triangle side c = "))
-    print("Perimeter of triangle is", per)
-    print("Area of triangle is", area)
+    print("Side length can not be negative!")
 else:
     print("Perimeter of triangle is", per)
     print("Area of triangle is", area)
