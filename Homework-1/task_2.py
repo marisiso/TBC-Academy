e00ffac6ed87a8759c3adf49ce49f2434d15d1aa
Mari <marisisoeva@gmail.com>
2024-03-04 18:28:17 +0400

    Fixed code

--- Homework-1/task_2.py
+++ Homework-1/task_2.py
@@ -1,4 +1,8 @@
 Name = input("Enter your name: ")
-Age = 2024 - int(input("Enter year you were born in: "))
+Year_born = int(input("Enter year you were born in: "))
+Age = 2024 - Year_born
 
-print("Hi,", Name, "you are", Age, "years old")
+if Year_born > 2024:
+    print('Year of birth can not be greater than 2024!')
+else:
+    print("Hi,", Name, "you are", Age, "years old")
